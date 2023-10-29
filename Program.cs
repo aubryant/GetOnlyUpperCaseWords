cc7c9de5d74dc8769a1b3b7993c2c619f5548cd1
aubryant <aubryant@CH67KK0J3>
2023-10-29 18:46:03 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿namespace TestingPurposes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Placeholder text
+        }
+
+        public List<string> GetOnlyUpperCaseWords(List<string> words)
+        {
+
+            // This method will iterate over a List of strings, evaluate if
+            // the word contains only valid & uppercase letters, checks for duplicate entries,
+            // and return a List of each unique word.
+
+            List<string> upperCaseList = new();
+
+            foreach (string word in words)
+            {
+                bool wordIsUpper = true;
+                bool containsSpecialCharacter = word.Any(c => !char.IsLetter(c));
+
+                if (containsSpecialCharacter == true)
+                {
+                    continue;
+                }
+
+                if (upperCaseList.Contains(word))
+                {
+                    continue;
+                }
+
+                foreach (char character in word)
+                {
+                    bool isUpperCase = char.IsUpper(character);
+
+                    if (!isUpperCase)
+                    {
+                        wordIsUpper = false;
+                        break;
+                    }
+                }
+
+                if (!wordIsUpper)
+                {
+                    continue;
+                }
+
+                upperCaseList.Add(word);
+            }
+
+            return upperCaseList;
+        }
+    }
+}
